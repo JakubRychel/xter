0f3f53fa64ae39dd348d5dfb769d6c42c5bf8f86
JakubRychel <rychelja@gmail.com>
2025-11-03 10:08:17 +0100

    improve post sending UI

--- frontend/src/components/CreatePostForm.js
+++ frontend/src/components/CreatePostForm.js
@@ -1,24 +1,43 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { createPost } from '../services/posts';
 
 function CreatePostForm({ parent=null, addToFeed }) {
   const [content, setContent] = useState('');
-
   const [error, setError] = useState('');
 
+  const [sending, setSending] = useState(false);
+  const [progress, setProgress] = useState('1%');
+
+  const timerRef = useRef(null);
+
   const submit = async () => {
-    const token = localStorage.getItem('access');
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+
+    setSending(true);
+
     const postData = { content, parent_id: parent };
 
     try {
-      const post = await createPost(postData, token);
+      const post = await createPost(postData);
       addToFeed(post);
-      setContent('');
+      setProgress('100%');
+
+      timerRef.current = setTimeout(() => {
+        setSending(false);
+        setContent('');
+        setProgress('1%');
+        timerRef.current = null;
+      }, 300);
     }
     catch (error) {
+      setSending(false);
+      setContent('');
+      setProgress('1%');
       setError(error.message);
     }
-  };
+  }
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -43,6 +62,19 @@ function CreatePostForm({ parent=null, addToFeed }) {
       </>)}
 
       <div className="my-3">
+
+        {sending && (<>
+          <div className="progress" role="progressbar" style={{height: '5px', marginBottom: '-5px'}}>
+            <div 
+              className="progress-bar"
+              style={{
+                width: `${progress}`,
+                transition: 'width .3s ease-in-out'
+              }}
+            ></div>
+          </div>
+        </>)}
+        
         <textarea
           placeholder="Napisz coś"
           className="form-control"
@@ -50,7 +82,9 @@ function CreatePostForm({ parent=null, addToFeed }) {
           value={content}
           onChange={event => setContent(event.target.value)}
           onKeyDown={handleKeyDown}
+          disabled={sending}
         />
+        
       </div>
       <div className="my-3">
         <button type="submit" className="btn btn-primary">Wyślij</button>
